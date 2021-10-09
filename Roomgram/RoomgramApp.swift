f8c0de6f7256b0d977bf1e9de4356349e3dda4ff
Sergey <sgolubnik@gmail.com>
2021-10-09 21:10:25 +0300

    Initial Commit

---
+++ Roomgram/RoomgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomgramApp.swift
+//  Roomgram
+//
+//  Created by СОВА on 09.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct RoomgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
